edb84e538422c8e7e22ad2bb09d800833e1282c5
dangtrantrung <trung280584@gmail.com>
2024-05-19 10:43:37 +0700

    Component By Manual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http'
 
-import { AppComponent } from './app.component';
-import { ComponentOverviewByCliComponent } from './component-overview-by-cli/component-overview-by-cli.component';
+import { AppComponent } from './app.component'
+import { ComponentOverviewByCliComponent } from './component-overview-by-cli/component-overview-by-cli.component'
+import { ComponentByManualComponent } from './component-by-manual/component-by-manual.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentOverviewByCliComponent
+    ComponentOverviewByCliComponent,
+    ComponentByManualComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
+  imports: [BrowserModule, FormsModule, HttpModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/component-by-manual/component-by-manual.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-component-by-manual',
+  templateUrl: './component-by-manual.component.html',
+  styleUrls: ['./component-by-manual.component.css'],
+})
+export class ComponentByManualComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
