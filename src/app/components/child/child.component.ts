7602cce3885ffc396f78819836792e92e5c4d5ec
dangtrantrung <trung280584@gmail.com>
2024-05-19 16:09:05 +0700

    AfterContentInit, AfterContentChecked, AfterViewInit

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,10 +1,16 @@
 import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewInit,
   Component,
+  ContentChild,
   DoCheck,
+  ElementRef,
   Input,
   OnChanges,
   OnInit,
   SimpleChanges,
+  ViewChild,
 } from '@angular/core'
 
 @Component({
@@ -12,15 +18,40 @@ import {
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css'],
 })
-export class ChildComponent implements OnChanges, OnInit, DoCheck {
+export class ChildComponent
+  implements
+    OnChanges,
+    OnInit,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit {
   ngOnInit(): void {
     // throw new Error('Method not implemented.')
     console.log('OnInit child component triggerred')
   }
   constructor() {}
+  ngAfterViewInit(): void {
+    // throw new Error('Method not implemented.')
+    console.log('AfterViewInit trigerred')
+  }
+  ngAfterContentChecked(): void {
+    // throw new Error('Method not implemented.')
+    console.log('AfterContentchecked trigerred')
+  }
+  ngAfterContentInit(): void {
+    // throw new Error('Method not implemented.')
+    console.log('ngaftercontetnInit-wrapper', this.wrapper)
+    console.log('ngaftercontetnInit-content wrapper', this.content)
+  }
+  @ViewChild('wrapper') wrapper: ElementRef
+  @ContentChild('contentWrapper') content: ElementRef
+
   ngDoCheck(): void {
     // throw new Error('Method not implemented.')
-    console.log('doCheck triggered from child component')
+    console.log(
+      'doCheck triggered from child component, not onchange triggered - reference object value but still Do check',
+    )
   }
   ngOnChanges(changes: SimpleChanges): void {
     // throw new Error('Method not implemented.')
--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from '@angular/core'
+import {
+  AfterContentInit,
+  Component,
+  ContentChild,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core'
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css'],
 })
-export class ParentComponent implements OnInit{
+export class ParentComponent implements OnInit {
   ngOnInit(): void {
     // throw new Error('Method not implemented.')
     console.log('OnInit parent component triggerred')
