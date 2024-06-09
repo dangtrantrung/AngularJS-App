51ae981afd501f202fd41e4a8241464865e84e88
dangtrantrung <trung280584@gmail.com>
2024-06-09 13:06:17 +0700

    class binding - stylesUrl

--- src/app/nav-bar.component.ts
+++ src/app/nav-bar.component.ts
@@ -8,11 +8,18 @@ import { Component } from '@angular/core'
       <a [style.text-decoration]="activeLinkStyle">Home Page</a>
       <a [style.text-decoration]="linkStyle">Login</a>
     </nav>
+    <div class="foo">
+      <p [class.bar]="true">class binding</p>
+    </div>
   `,
+  styleUrls: ['./app.component.css'],
 })
 export class NavBarComponent {
   navStyle = 'font-size: 1.2rem; color: cornflowerblue;'
   linkStyle = 'underline'
   activeLinkStyle = 'overline'
+  //   foo = 'color: blue;'
+  //   bar = 'background-color: aqua;'
+
   /* . . . */
 }
